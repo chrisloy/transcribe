ac9a8937bd3b37b077c5ac9c7e5029b972bc83c2
Chris Loy <chrisloyuk@gmail.com>
2016-06-06 18:59:09 +0100

    93442

--- train.py
+++ train.py
@@ -1,16 +1,16 @@
-import generate
 import midi
 import numpy as np
-from os import devnull
 import slicer
 import spectrogram
 import sys
 import tensorflow as tf
+from sklearn.metrics import roc_curve
+from matplotlib import pyplot as plt
 
 
-def load_x(wav_file, slices):
-    _, _, data = spectrogram.load_slice(wav_file, slices)
-    return data[:, 0:slices]  # TODO why do I have to trim the end?
+def load_x(wav_file, slice_samples):
+    _, ts, data = spectrogram.spectrogram_10hz(wav_file, slice_samples)
+    return data, ts.shape[0]
 
 
 def load_y(midi_file, slices):
@@ -18,13 +18,17 @@ def load_y(midi_file, slices):
     return slicer.slice_midi_into(m, slices)
 
 
-def load_slices(a, b):
+def load_slices(a, b, slice_samples):
     x = []
     y = []
     for i in range(a, b):
-        s = 2000
+
+        sys.stdout.write("%d/%d\r" % (i - a, b - a))
+        sys.stdout.flush()
+
+        xi, s = load_x("output/%04d.wav" % i, slice_samples)
         yi = load_y("output/%04d.mid" % i, s)
-        xi = load_x("output/%04d.wav" % i, s)
+
         if xi.shape[1] == yi.shape[1]:
             x.append(xi)
             y.append(yi)
@@ -35,63 +39,97 @@ def load_slices(a, b):
     return np.transpose(x), np.transpose(y)
 
 
+def param(shape):
+    return tf.Variable(tf.random_normal(shape, 0.35))
+
+
 def run_logistic_regression():
     sess = tf.InteractiveSession()
 
-    x = tf.placeholder(tf.float32, shape=[None, 163])
+    slice_samples = 4410
+    features = 882
+
+    x = tf.placeholder(tf.float32, shape=[None, features])
     y_ = tf.placeholder(tf.float32, shape=[None, 128])
 
-    w1 = tf.Variable(tf.zeros([163, 163]))
-    b1 = tf.Variable(tf.zeros([163]))
+    w1 = param([features, 128])
+    b1 = param([128])
     h1 = tf.nn.sigmoid(tf.matmul(x, w1) + b1)
 
-    w2 = tf.Variable(tf.zeros([163, 163]))
-    b2 = tf.Variable(tf.zeros([163]))
-    h2 = tf.nn.sigmoid(tf.matmul(h1, w2) + b2)
+    # w2 = param([features, features])
+    # b2 = param([features])
+    # h2 = tf.nn.sigmoid(tf.matmul(h1, w2) + b2)
 
-    w3 = tf.Variable(tf.zeros([163, 128]))
-    b3 = tf.Variable(tf.zeros([128]))
-    h3 = tf.nn.sigmoid(tf.matmul(h2, w3) + b3)
+    # w3 = param([features, 128])
+    # b3 = param([128])
+    # h3 = tf.nn.sigmoid(tf.matmul(h2, w3) + b3)
 
-    y = h3
+    y = h1
+
+    mse = tf.reduce_sum(tf.abs(y - y_))
+
+    # train_step = tf.train.GradientDescentOptimizer(2.5).minimize(mse)
+    train_step = tf.train.AdamOptimizer().minimize(mse)
 
     sess.run(tf.initialize_all_variables())
 
-    mse = tf.reduce_mean(tf.square(y - y_))
-    train_step = tf.train.GradientDescentOptimizer(0.1).minimize(mse)
+    epochs = 2000
 
-    for j in range(2):
-        for i in range(400):
+    print "Loading training set...."
+    x_train, y_train = load_slices(0, 900, slice_samples)
 
-            sys.stdout.write("Batch %d %d\r" % (j, i))
+    print "Loading testing set...."
+    x_test, y_test = load_slices(900, 1000, slice_samples)
+
+    batch_size = 1000
+    batches = x_train.shape[0] / batch_size
+
+    for j in range(epochs):
+
+        for i in range(batches):
+
+            sys.stdout.write("EPOCH %d/%d ... BATCH %d/%d\r" % (j + 1, epochs, i + 1, batches))
             sys.stdout.flush()
 
-            start = i*2
-            stop = (i+1)*2
-            x_train, y_train = load_slices(start, stop)
+            start = i * batch_size
+            stop = (i + 1) * batch_size
 
-            train_step.run(feed_dict={x: x_train, y_: y_train})
+            train_step.run(feed_dict={x: x_train[start:stop, :], y_: y_train[start:stop, :]})
 
-            if i % 100 == 0:
-                print "MSE: ", mse.eval(feed_dict={x: x_train, y_: y_train})
+        if j % 20 == 0:
+            sys.stdout.write("EPOCH %d  ...  TRAIN ERROR: %0.4f  ...  TEST ERROR: %0.4f\n" %
+                             (
+                                 j + 1,
+                                 mse.eval(feed_dict={x: x_train, y_: y_train}),
+                                 mse.eval(feed_dict={x: x_test, y_: y_test})
+                             )
+            )
+            sys.stdout.flush()
+        else:
+            sys.stdout.write("\n")
+            sys.stdout.flush()
 
-    x_test, y_test = load_slices(900, 1000)
-    print "TEST MSE:", mse.eval(feed_dict={x: x_test, y_: y_test})
+    y_pred = y.eval(feed_dict={x: x_test})
+    fpr, tpr, thresholds = roc_curve(y_test.flatten(), y_pred.flatten())
 
-    ex = np.transpose(load_x("output/sanity.wav", 2000))
+    plt.plot(fpr, tpr)
+    plt.show()
 
-    output = np.transpose(y.eval(feed_dict={x: ex}))
-    print output.shape
-    track = slicer.boolean_table_to_track(output)
-
-    print track
-    original_track = midi.read_midifile("output/sanity.mid")[0]
-    print original_track
-
-    pattern = midi.Pattern()
-    pattern.append(track)
-    midi.write_midifile("output/sanity_pred.mid", pattern)
-    generate.write_wav_file("output/sanity_pred.mid", "output/sanity_pred.wav", open(devnull, 'w'))
+    # ex, _ = load_x("output/sanity.wav", slice_samples)
+    # ex = np.transpose(ex)
+    #
+    # output = np.transpose(y.eval(feed_dict={x: ex}))
+    # print output.shape
+    # track = slicer.boolean_table_to_track(output)
+    #
+    # print track
+    # original_track = midi.read_midifile("output/sanity.mid")[0]
+    # print original_track
+    #
+    # pattern = midi.Pattern()
+    # pattern.append(track)
+    # midi.write_midifile("output/sanity_pred_deep.mid", pattern)
+    # generate.write_wav_file("output/sanity_pred.mid", "output/sanity_pred_deep.wav", open(devnull, 'w'))
 
 
 if __name__ == "__main__":
