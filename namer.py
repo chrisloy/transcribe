939b428bfee6c0c7129db084fd6dd66e81e2de62
Chris Loy <chrisloyuk@gmail.com>
2016-07-25 14:38:29 +0100

    Namer tidyup

--- namer.py
+++ namer.py
@@ -3,8 +3,5 @@ import random
 
 def new_name():
     with open("/usr/share/dict/words") as f:
-        words = f.readlines()
-        print len(words)
-        words = filter(lambda w: 4 <= len(w) <= 10, words)
-        print len(words)
+        words = filter(lambda w: 4 <= len(w) <= 10, f.readlines())
         return "%s-%s" % (random.choice(words).strip().lower(), random.choice(words).strip().lower())
