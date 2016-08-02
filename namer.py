b162714c8110e7d25592c785dc4b62528452fc09
Chris Loy <chrisloyuk@gmail.com>
2016-08-02 11:20:09 +0100

    Non-deterministic naming

--- namer.py
+++ namer.py
@@ -1,7 +1,10 @@
 import random
 
 
+r = random.Random()
+
+
 def new_name():
     with open("/usr/share/dict/words") as f:
         words = filter(lambda w: 4 <= len(w) <= 10, f.readlines())
-        return "%s-%s" % (random.choice(words).strip().lower(), random.choice(words).strip().lower())
+        return "%s-%s" % (r.choice(words).strip().lower(), r.choice(words).strip().lower())
