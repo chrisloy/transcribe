ab452e700cdfc0378dc155c8076482587cbc3253
Chris Loy <chrisloyuk@gmail.com>
2016-09-03 16:45:37 +0100

    Split graphs into separate module

---
+++ graphs.py
@@ -0,0 +1,99 @@
+import tensorflow as tf
+
+
+def param_norm(shape, name):
+    return tf.Variable(tf.truncated_normal(shape, 0.1, seed=1234), dtype="float32", name=name)
+
+
+def param_zeros(shape, name):
+    return tf.Variable(tf.zeros(shape), dtype="float32", name=name)
+
+
+def deep_neural_network(input_tensor, layers, dropout=None):
+
+    assert len(layers) >= 2
+
+    act = None
+    trans = input_tensor
+
+    print "Graph shape: %s" % " --> ".join(map(str, layers))
+
+    for i, nodes in enumerate(layers[1:]):
+
+        w = param_norm([layers[i-1], nodes], "W%d" % i)
+        b = param_norm([nodes], "b%d" % i)
+
+        act = tf.matmul(trans, w) + b
+
+        if i + 1 < len(layers):
+            trans = tf.nn.relu(act)
+            if dropout:
+                trans = tf.nn.dropout(trans, dropout, seed=1)
+
+    return act
+
+
+def recurrent_neural_network(
+        input_tensor,
+        input_size,
+        output_size,
+        steps,
+        hidden,
+        graph_type):
+
+    initial_state = rnn_initial_state(graph_type, hidden)
+
+    weights = {
+        'hidden': tf.Variable(tf.random_normal([input_size, hidden])),
+        'out': tf.Variable(tf.random_normal([hidden, output_size]))
+    }
+    biases = {
+        'hidden': tf.Variable(tf.random_normal([hidden])),
+        'out': tf.Variable(tf.random_normal([output_size]))
+    }
+
+    w_h = param_norm([input_size, hidden], "w_h")
+    b_o = param_norm([output_size], "b_o")
+    w_o = param_norm([hidden, output_size], "w_o")
+    b_h = param_norm([hidden], "b_h")
+
+    input_layer = input_tensor                                       # (batch, steps, input)
+    input_layer = tf.transpose(input_layer, [1, 0, 2])               # (steps, batch, input)
+    input_layer = tf.reshape(input_layer, [-1, input_size])          # (steps * batch, input)
+    input_layer = tf.matmul(input_layer, w_h) + b_h                  # (steps * batch, hidden)
+    input_layer = tf.split(0, steps, input_layer)                    # (steps, batch, hidden)
+
+    cell = rnn_cell(graph_type, hidden)                              # (steps, batch, hidden)
+
+    output_layer = rnn(graph_type, cell, initial_state, input_layer) # (steps, batch, hidden)
+    output_layer = tf.reshape(output_layer, [-1, hidden])            # (steps * batch, hidden)
+    output_layer = tf.matmul(output_layer, w_o) + b_o                # (steps * batch, output)
+    output_layer = tf.split(0, steps, output_layer)                  # (steps, batch, output)
+
+    return tf.transpose(output_layer, [1, 0, 2]), initial_state                 # (batch, steps, output)
+
+
+def rnn_initial_state(graph_type, hidden):
+    if graph_type == 'lstm' or graph_type == 'bi_lstm':
+        return tf.placeholder(tf.float32, shape=[None, hidden * 2], name="initial_state")
+    else:
+        return tf.placeholder(tf.float32, shape=[None, hidden], name="initial_state")
+
+
+def rnn_cell(graph_type, size):
+    if graph_type == 'lstm' or graph_type == 'bi_lstm':
+        return tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=1.0)
+    elif graph_type == 'gru' or 'bi_gru':
+        return tf.nn.rnn_cell.GRUCell(size)
+    else:
+        return tf.nn.rnn_cell.BasicRNNCell(size)
+
+
+def rnn(graph_type, cell, initial_state, x):
+    if graph_type == 'bi_rnn' or graph_type == 'bi_lstm' or graph_type == 'bi_gru':
+        outputs, _, _ = tf.nn.bidirectional_rnn(cell, cell, x, initial_state, initial_state)
+        fw, bw = tf.split(2, 2, outputs)
+        output = fw
+    else:
+        output, _ = tf.nn.rnn(cell, x, initial_state)
+    return output
--- model.py
+++ model.py
@@ -1,4 +1,4 @@
-import sys
+import graphs
 import tensorflow as tf
 
 
@@ -22,68 +22,22 @@ class Model:
         return tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
 
 
-def param_norm(shape, name):
-    return tf.Variable(tf.truncated_normal(shape, 0.1, seed=1234), dtype="float32", name=name)
-
-
-def param_zeros(shape, name):
-    return tf.Variable(tf.zeros(shape), dtype="float32", name=name)
-
-
 def feed_forward_model(
         features,
         output,
         learning_rate=0.001,
         hidden_nodes=list(),
-        dropout=False,
-        one_hot=False,
-        with_bias=True):
+        dropout=None,
+        one_hot=False):
 
     tf.set_random_seed(1)
 
     x = tf.placeholder(tf.float32, shape=[None, features], name="x")
     y_gold = tf.placeholder(tf.float32, shape=[None, output], name="y_gold")
-    previous_nodes = features
-    act = None
-    trans = x
+    act = graphs.deep_neural_network(x, [features] + hidden_nodes + [output], dropout)
+    y, loss = y_and_loss(act, y_gold, one_hot)
 
-    depth = 0
-
-    sys.stdout.write("Graph shape: %d" % previous_nodes)
-
-    layers = hidden_nodes + [output]
-
-    for i, nodes in enumerate(layers):
-
-        sys.stdout.write(" --> %d" % nodes)
-        depth += 1
-        w = param_norm([previous_nodes, nodes], "W%d" % depth)
-
-        if with_bias:
-            b = param_norm([nodes], "b%d" % depth)
-            act = tf.matmul(trans, w) + b
-        else:
-            act = tf.matmul(trans, w)
-
-        if i + 1 < len(layers):
-            trans = tf.nn.relu(act)
-            if dropout:
-                trans = tf.nn.dropout(trans, 0.5, seed=1)
-
-        previous_nodes = nodes
-
-    sys.stdout.write("\n")
-
-    if one_hot:
-        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(act, y_gold))
-        y = tf.nn.softmax(act, name="y")
-    else:
-        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(act, y_gold))
-        y = tf.nn.sigmoid(act, name="y")
-
-    train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-
-    return Model(x, y, y_gold, loss, train_step)
+    return Model(x, y, y_gold, loss, train(loss, learning_rate))
 
 
 def rnn_model(
@@ -98,62 +52,21 @@ def rnn_model(
 
     x = tf.placeholder(tf.float32, shape=[None, steps, features], name="x")
     y_gold = tf.placeholder(tf.float32, shape=[None, steps, notes], name="y_gold")
-    initial_state = rnn_initial_state(graph_type, hidden)
+    logits, i_state = graphs.recurrent_neural_network(x, features, notes, steps, hidden, graph_type)
+    y, loss = y_and_loss(logits, y_gold, False)
 
-    weights = {
-        'hidden': tf.Variable(tf.random_normal([features, hidden])),
-        'out': tf.Variable(tf.random_normal([hidden, notes]))
-    }
-    biases = {
-        'hidden': tf.Variable(tf.random_normal([hidden])),
-        'out': tf.Variable(tf.random_normal([notes]))
-    }
-
-    def make_graph(_x, _weights, _biases):                            # (batch, steps, feats)
-        _x = tf.transpose(_x, [1, 0, 2])                              # (steps, batch, feats)
-        _x = tf.reshape(_x, [-1, features])                           # (steps * batch, feats)
-        _x = tf.matmul(_x, _weights['hidden']) + _biases['hidden']    # (steps * batch, hidden)
-        _x = tf.split(0, steps, _x)                                   # (steps, batch, hidden)
-
-        cell = rnn_cell(graph_type, hidden)                           # (steps, batch, hidden)
-
-        output = rnn(graph_type, cell, initial_state, _x)             # (steps, batch, hidden)
-        output = tf.reshape(output, [-1, hidden])                     # (steps * batch, hidden)
-        output = tf.matmul(output, _weights["out"]) + _biases["out"]  # (steps * batch, notes)
-        output = tf.split(0, steps, output)                           # (steps, batch, notes)
-
-        return tf.transpose(output, [1, 0, 2])                        # (batch, steps, notes)
-
-    log_y = make_graph(x, weights, biases)
-
-    loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(log_y, y_gold))
-    train_step = tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
-    y = tf.sigmoid(log_y)
-
-    return Model(x, y, y_gold, loss, train_step, initial_state)
+    return Model(x, y, y_gold, loss, train(loss, learning_rate), i_state)
 
 
-def rnn_initial_state(graph_type, hidden):
-    if graph_type == 'lstm' or graph_type == 'bi_lstm':
-        return tf.placeholder(tf.float32, shape=[None, hidden * 2], name="initial_state")
+def train(loss, learning_rate):
+    return tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
+
+
+def y_and_loss(logits, y_gold, one_hot):
+    if one_hot:
+        loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(logits, y_gold))
+        y = tf.nn.softmax(logits, name="y")
     else:
-        return tf.placeholder(tf.float32, shape=[None, hidden], name="initial_state")
-
-
-def rnn_cell(graph_type, size):
-    if graph_type == 'lstm' or graph_type == 'bi_lstm':
-        return tf.nn.rnn_cell.BasicLSTMCell(size, forget_bias=1.0)
-    elif graph_type == 'gru' or 'bi_gru':
-        return tf.nn.rnn_cell.GRUCell(size)
-    else:
-        return tf.nn.rnn_cell.BasicRNNCell(size)
-
-
-def rnn(graph_type, cell, initial_state, x):
-    if graph_type == 'bi_rnn' or graph_type == 'bi_lstm' or graph_type == 'bi_gru':
-        outputs, _, _ = tf.nn.bidirectional_rnn(cell, cell, x, initial_state, initial_state)
-        fw, bw = tf.split(2, 2, outputs)
-        output = fw
-    else:
-        output, _ = tf.nn.rnn(cell, x, initial_state)
-    return output
+        loss = tf.reduce_mean(tf.nn.sigmoid_cross_entropy_with_logits(logits, y_gold))
+        y = tf.nn.sigmoid(logits, name="y")
+    return y, loss
