848a81d5e82224355c1efca0f83533f91e47ba3e
Chris Loy <chrisloyuk@gmail.com>
2016-09-08 19:10:25 +0100

    Data improvements

--- data.py
+++ data.py
@@ -38,15 +38,6 @@ class Data:
         self.y_test = y_test
         self.n_test = x_test.shape[0]
 
-    def to_normalised(self):
-        print np.min(self.x_train), np.max(self.x_train), np.mean(self.x_train), np.var(self.x_train)
-        mean = np.mean(self.x_train, axis=0)
-        var = np.var(self.x_train, axis=0)
-        print np.min(mean), np.max(mean), np.min(var), np.max(var)
-        self.x_train = (self.x_train - mean) / var
-        self.x_test = (self.x_test - mean) / var
-        return self
-
     def to_binary_one_hot(self):
         # Change y labels into one-hot vectors in two dimensions.
         y_train = np.stack([1 - self.y_train, self.y_train], axis=2)
@@ -120,6 +111,9 @@ class Data:
         self.n_train = self.x_train.shape[0]
         self.n_test = self.x_test.shape[0]
         self.batches = self.n_train / self.batch_size
+
+        print "Separated data into [%d] train and [%d] test sequences of length [%d]" %\
+              (self.n_train, self.n_test, sequence_length)
 
         return self
 
