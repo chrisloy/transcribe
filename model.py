a2cbf9a2c83547a77b7e292cf8377818ad9b5ca1
Chris Loy <chrisloyuk@gmail.com>
2016-09-15 10:35:39 +0100

    Hybrid model with no transfer layers

--- model.py
+++ model.py
@@ -127,6 +127,68 @@ def hybrid_model(
     return acoustic, sequence
 
 
+def hybrid_model_no_transfers(
+        features,
+        notes,
+        steps,
+        acoustic_hidden_nodes,
+        rnn_type,
+        rnn_learning_rate,
+        acoustic_learning_rate,
+        dropout=None,
+        one_hot=False,
+        freeze_frame_model=True):
+
+    tf.set_random_seed(1)
+
+    assert acoustic_hidden_nodes is not None
+
+    x = tf.placeholder(tf.float32, shape=[None, steps, features], name="x_sequence")
+    y_gold = tf.placeholder(tf.float32, shape=[None, steps, notes], name="y_sequence_gold")
+
+    x_acoustic = x                                              # (batch, steps, features)
+    x_acoustic = tf.transpose(x_acoustic, [1, 0, 2])            # (steps, batch, features)
+    x_acoustic = tf.reshape(x_acoustic, [-1, features])         # (steps * batch, features)
+
+    y_acoustic_gold = y_gold                                    # (batch, steps, notes)
+    y_acoustic_gold = tf.transpose(y_acoustic_gold, [1, 0, 2])  # (steps, batch, notes)
+    y_acoustic_gold = tf.reshape(y_acoustic_gold, [-1, notes])  # (steps * batch, notes)
+
+    # Acoustic Model
+    layers = [features] + acoustic_hidden_nodes + [notes]
+
+    logits_acoustic_fixed, _ = graphs.deep_neural_network(x_acoustic, layers, dropout)
+    logits_acoustic = logits_acoustic_fixed
+    y_acoustic, loss_acoustic = y_and_loss(logits_acoustic, y_acoustic_gold, one_hot)
+
+    if freeze_frame_model:
+        frozen_acoustic = tf.stop_gradient(logits_acoustic_fixed)
+    else:
+        frozen_acoustic = logits_acoustic_fixed
+
+    train_acoustic = train(loss_acoustic, acoustic_learning_rate)
+    acoustic = Model(x, y_acoustic, y_gold, loss_acoustic, train_acoustic)
+
+    def transfer_layer(not_used):
+        return frozen_acoustic
+
+    # Sequence Model
+    sequence = graphs.rnn_no_layers(
+        x,
+        notes,
+        steps,
+        rnn_type,
+        transfer_layer
+    )
+
+    y_sequence, loss_sequence = y_and_loss(sequence, y_gold, one_hot)
+    train_sequence = train(loss_sequence, rnn_learning_rate)
+
+    sequence = Model(x, y_sequence, y_gold, loss_sequence, train_sequence)
+
+    return acoustic, sequence
+
+
 def train(loss, learning_rate):
     return tf.train.AdamOptimizer(learning_rate=learning_rate).minimize(loss)
 
