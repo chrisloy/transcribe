1287b2dbd1ee217bf8af7fb6e9909e6b137a4648
Chris Loy <chrisloyuk@gmail.com>
2016-09-15 10:32:20 +0100

    RNN no layers

--- graphs.py
+++ graphs.py
@@ -43,6 +43,27 @@ def logistic_regression(input_tensor, input_size, output_size):
     return y
 
 
+def rnn_no_layers(input_tensor, size, steps, graph_type, input_model):
+
+    # TODO this probably doesn't work with a bi-directional RNN
+
+    rnn_out = 2 * size if graph_type.startswith('bi_') else size
+
+    input_layer = input_tensor                                        # (batch, steps, input)
+    input_layer = tf.transpose(input_layer, [1, 0, 2])                # (steps, batch, input)
+    input_layer = tf.reshape(input_layer, [-1, size])                 # (steps * batch, input)
+    input_layer = input_model(input_layer)                            # (steps * batch, state)
+    input_layer = tf.split(0, steps, input_layer)                     # (steps, batch, state)
+
+    cell = rnn_cell(graph_type, size)                                 # (steps, batch, state)
+
+    output_layer = rnn(graph_type, cell, input_layer)                 # (steps * 2, batch, state)
+    output_layer = tf.reshape(output_layer, [-1, rnn_out])            # (steps * batch, hidden * 2)
+    output_layer = tf.split(0, steps, output_layer)                   # (steps, batch, output)
+
+    return tf.transpose(output_layer, [1, 0, 2])                      # (batch, steps, output)
+
+
 def recurrent_neural_network(
         input_tensor,
         input_size,
