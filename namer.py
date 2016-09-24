fc59825080a95a18411c4d66d8547cae8dcf5bfe
Chris Loy <chrisloyuk@gmail.com>
2016-09-24 14:03:32 +0100

    Removed non alphanumeric characters from graph names

--- namer.py
+++ namer.py
@@ -1,10 +1,15 @@
 import random
+import re
 
 
 r = random.Random()
 
 
+def clean(word):
+    return re.sub(r'[^a-zA-Z0-9]','', word).strip().lower()
+
+
 def new_name():
     with open("/usr/share/dict/words") as f:
         words = filter(lambda w: 4 <= len(w) <= 10, f.readlines())
-        return "%s-%s" % (r.choice(words).strip().lower(), r.choice(words).strip().lower())
+        return "%s-%s" % (clean(r.choice(words)), clean(r.choice(words)))
